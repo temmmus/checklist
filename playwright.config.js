9bb5e28d4f6d2fea0e6afa30f020791cdd4ab0e9
temmmus <temmmus@gmail.com>
2024-11-14 00:53:16 +0700

    chore: update playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -2,4 +2,10 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "tests/playwright",
+  retries: 1,
+  reporter: [["html", { open: "never" }]],
+  use: {
+    headless: true,
+    screenshot: "only-on-failure",
+  },
 });
